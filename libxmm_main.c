b61afb310a4922bbf56733cb4bf4b6d6b9a0556d
pithlessly <79002325+pithlessly@users.noreply.github.com>
2023-10-06 11:07:53 -0700

    clean up top-level C module

--- libxmm_main.c
+++ libxmm_main.c
@@ -5,33 +5,27 @@
 extern void zig_hello(void);
 
 extern PyObject *apply_subst(PyObject *, PyObject *);
-static const PyCFunction m_apply_subst = apply_subst;
 
 static struct PyMethodDef methods[] = {
     {
         .ml_name = "apply_subst",
-        .ml_meth = m_apply_subst,
+        .ml_meth = apply_subst,
         .ml_flags = METH_VARARGS,
         .ml_doc = NULL,
     },
-    {
-        .ml_name = NULL,
-        .ml_meth = NULL,
-        .ml_flags = 0,
-        .ml_doc = NULL,
-    }
+    { NULL }
 };
 
 static struct PyModuleDef module = {
-    .m_base = PyModuleDef_HEAD_INIT,
-    .m_name = "libxmm",
-    .m_doc = NULL,
-    .m_size = -1,
-    .m_methods = methods,
-    .m_slots = NULL,
+    .m_base     = PyModuleDef_HEAD_INIT,
+    .m_name     = "libxmm",
+    .m_doc      = NULL,
+    .m_size     = -1,
+    .m_methods  = methods,
+    .m_slots    = NULL,
     .m_traverse = NULL,
-    .m_clear = NULL,
-    .m_free = NULL,
+    .m_clear    = NULL,
+    .m_free     = NULL,
 };
 
 PyMODINIT_FUNC
